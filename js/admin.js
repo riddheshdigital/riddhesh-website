1e3413858b6979a7cd1d4c1f231e650284b46cbf
riddheshdigital <riddheshdigital@gmail.com>
2026-01-04 12:07:38 +0530

    fix admin

--- js/admin.js
+++ js/admin.js
@@ -8,7 +8,4 @@ function saveContent() {
   alert("Content Updated!");
 }
 
-const pass = prompt("Enter admin password");
-if (pass !== "admin123") {
-  document.body.innerHTML = "Access Denied";
-}
+
